6ef32374cccd1c88404cb98adf8543930957f83f
abdykerimov <abdykerimov.urm@gmail.com>
2022-03-07 21:16:21 +0300

    yandex-ui added, api url fixed

--- src/API/ThesisAPI.js
+++ src/API/ThesisAPI.js
@@ -1,13 +1,20 @@
 import axios from "axios";
+var apiUrl = "http://84.252.140.218/api";
+if(process.env.NODE_ENV != "development") {
+    apiUrl = "http://84.252.140.218/api";
+} else {
+    apiUrl = "https://localhost:44312";
+}
 
 export default class ThesisAPIService {
+    
     static async getAll(limit = 10, page = 1) {
-        var response = await axios.get('https://localhost:44312/api/Departments');
+        var response = await axios.get(`${apiUrl}/Departments`);
         return response;
     }
     
     static async getById(id) {
-        var response = await axios.get('https://localhost:44312/api/Departments/' + id);
+        var response = await axios.get(`${apiUrl}/Departments/${id}`);
         return response;
     }
     
