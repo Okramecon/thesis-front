3eba2fc528980ebb5d73dcff4430ed3f2a2355c5
abdykerimov <abdykerimov.urm@gmail.com>
2022-03-11 23:44:01 +0300

    fixed login page, alert added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { Drawer } from '@mui/material';
+import { Box, Drawer } from '@mui/material';
 import React, { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter';
+import GlobalAlert from './components/GlobalAlert';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import './styles/App.css';
 
 function App() {
-  const [sidebarVisible, setSidebarVisible] = useState(false);
-
+  const [sidebarVisible, setSidebarVisible] = useState(false)
+  const [alertState, setAlertState] = useState({loginAlertOpen: false, message: '', severity: 'success'})
+  const { loginAlertOpen, message, severity } = alertState
   const showHideSidebar = (prevVisibleState) => {
     setSidebarVisible(!prevVisibleState);
   }
@@ -17,19 +19,27 @@ function App() {
     <div className='App'>
       <BrowserRouter>
         <Navbar showHideSidebar={showHideSidebar} />
+
+        <Drawer anchor='left' open={sidebarVisible} onClose={showHideSidebar}>
+          <Sidebar setSidebarVisible={setSidebarVisible}/>
+        </Drawer>
+
         <div className='main'>
-          <Drawer
-            anchor='left'
-            open={sidebarVisible}
-            onClose={showHideSidebar}
-          >
-            <Sidebar setSidebarVisible={setSidebarVisible}/>
-          </Drawer>
-          <AppRouter/>
+          <Box style={{ flexGrow: 1, display: 'flex' }}>
+            <AppContext.Provider value={setAlertState}>
+              <AppRouter/>
+            </AppContext.Provider>
+            <GlobalAlert 
+              loginAlertOpen={loginAlertOpen} 
+              closeAlert={() => setAlertState({ ...alertState, loginAlertOpen: false})} 
+              message={message} severity={severity}/>
+          </Box>
         </div>
       </BrowserRouter>
     </div>
   );
 }
+
+export const AppContext = React.createContext();
 
 export default App;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,47 +1,66 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import TextField from '@mui/material/TextField';
-import { Button, Stack } from '@mui/material';
+import { Button, Grid, Paper, Stack, Box, Snackbar, Alert } from '@mui/material';
 import ThesisAPIService from '../API/ThesisAPI';
+import { useNavigate } from 'react-router-dom';
+import { AppContext } from '../App';
+import AlertSeverities from '../helpers/AlertSeverities';
 
 function LoginPage() {
   const [loginCredentials, setLoginCredentials] = useState({login:'', password:''})
   const {login, password} = loginCredentials
-
+  const navigate = useNavigate()
+  const setAlertState = useContext(AppContext)
   const loginWithCredentials = () => {
-    ThesisAPIService.loginWithCredentials(loginCredentials)
+    /*const bearerToken = ThesisAPIService.loginWithCredentials(loginCredentials)
+    localStorage.setItem('bearer', bearerToken)*/
+    setAlertState({ loginAlertOpen: true, message: 'Successfully logged in!', severity: AlertSeverities.success})
+    navigate('/')
   }
 
   return (
-    <Stack 
-      component="form"
-      sx={{
-       '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-      alignItems="center"
-      direction="column"
-      display="flex"
-    >
-      <TextField 
-        id="1" 
-        required
-        label="Login"
-        value={login}
-        onChange={e => setLoginCredentials({...loginCredentials, login:e.target.value})}
-      />
-      <TextField 
-        id="2"
-        required
-        label="Password"
-        value={password}
-        type="password"
-        autoComplete="current-password"
-        onChange={e => setLoginCredentials({...loginCredentials, password:e.target.value})}
-      />
-      <Button color="inherit" onClick={() => loginWithCredentials()}>Login</Button>
-      <p>АКРАМ Я НЕ ЗНАЮ КАК ВЫРОВНЯТЬ ФОРМУ ПО СЕРЕДИНЕ </p>
-    </Stack>
+    <React.Fragment>
+      <Box style={{
+        flexGrow: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <Paper 
+          style={{
+            padding: 25
+          }}
+          variant="elevation"
+          elevation={5}
+        >
+          <Grid container direction='column' spacing={2}>
+            <Grid item>
+              <TextField 
+                id="1" 
+                required
+                label="Login"
+                value={login}
+                onChange={e => setLoginCredentials({...loginCredentials, login:e.target.value})}
+              />
+            </Grid>
+            <Grid item>
+              <TextField 
+                id="2"
+                required
+                label="Password"
+                value={password}
+                type="password"
+                autoComplete="current-password"
+                onChange={e => setLoginCredentials({...loginCredentials, password:e.target.value})}
+              />
+            </Grid>
+            <Grid item>
+                <Button variant="contained" onClick={() => loginWithCredentials()}>Login</Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Box>
+    </React.Fragment>
   )
 }
 
